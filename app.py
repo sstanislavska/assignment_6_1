f5646dcc2540e631260e9033d1cbb1da392406fa
Svitlana Stanislavska <sstanislavskaya@gmail.com>
2024-03-30 00:36:28 +0100

    added multiplication

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def subtract(x, y):
     return x - y
 
 
+def multiplicate(x, y):
+    return x * y
+
+
 parser = argparse.ArgumentParser(description="CLI app - a simple calculator")
 parser.add_argument("x", type=float, help="Input x")
 parser.add_argument("y", type=float, help="Input y")
@@ -18,3 +22,4 @@ args = parser.parse_args()
 
 print(f"x + y = {add(args.x, args.y)}")
 print(f"x - y = {subtract(args.x, args.y)}")
+print(f"x * y = {multiplicate(args.x, args.y)}")
