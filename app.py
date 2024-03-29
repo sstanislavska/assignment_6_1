a9ee96f88fc9709729d5a531e6fd1c786c7fd6b6
Svitlana Stanislavska <sstanislavskaya@gmail.com>
2024-03-30 00:41:36 +0100

    added the division

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ def subtract(x, y):
     return x - y
 
 
+def divide(x, y):
+    if y == 0:
+        print("You can't divide by zero")
+        exit()
+    return x / y
+
+
 parser = argparse.ArgumentParser(description="CLI app - a simple calculator")
 parser.add_argument("x", type=float, help="Input x")
 parser.add_argument("y", type=float, help="Input y")
@@ -18,3 +25,4 @@ args = parser.parse_args()
 
 print(f"x + y = {add(args.x, args.y)}")
 print(f"x - y = {subtract(args.x, args.y)}")
+print(f"x / y = {divide(args.x, args.y)}")
