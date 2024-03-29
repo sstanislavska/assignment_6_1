b48e4f4bd9868b761ce5b39fadc8f979f9a341c2
Svitlana Stanislavska <sstanislavskaya@gmail.com>
2024-03-29 23:58:50 +0100

    Initial commit. CLI app - a simple calculator.

---
+++ app.py
@@ -0,0 +1,20 @@
+# CLI app - a simple calculator
+
+import argparse
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+parser = argparse.ArgumentParser(description="CLI app - a simple calculator")
+parser.add_argument("x", type=float, help="Input x")
+parser.add_argument("y", type=float, help="Input y")
+args = parser.parse_args()
+
+print(f"x + y = {add(args.x, args.y)}")
+print(f"x - y = {subtract(args.x, args.y)}")
